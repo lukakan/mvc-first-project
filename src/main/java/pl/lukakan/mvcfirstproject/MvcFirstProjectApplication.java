7067131123d90e5312e8c7dda55f11d0af03ab1b
Lukasz K <kanicki.lukasz@gmail.com>
2021-03-21 19:12:01 +0100

    initial commit

---
+++ src/main/java/pl/lukakan/mvcfirstproject/MvcFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.lukakan.mvcfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcFirstProjectApplication.class, args);
+    }
+
+}
